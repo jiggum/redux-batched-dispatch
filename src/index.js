405418041eb4895936baf173d538020eacdfa0c4
min <0s.dongmin@gmail.com>
2019-05-12 11:15:14 +0900

    Introduce Prettier & Husky

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ export default function reduxBatch(next) {
 
   function notifyListeners() {
     currentListeners = nextListeners
-    currentListeners.forEach((listener) => {
+    currentListeners.forEach(listener => {
       listener()
     })
   }
@@ -28,9 +28,9 @@ export default function reduxBatch(next) {
     if (isDispatching) {
       throw new Error(
         'You may not call store.subscribe() while the reducer is executing. ' +
-        'If you would like to be notified after the store has been updated, subscribe from a ' +
-        'component and invoke store.getState() in the callback to access the latest state. ' +
-        'See https://redux.js.org/api-reference/store#subscribe(listener) for more details.',
+          'If you would like to be notified after the store has been updated, subscribe from a ' +
+          'component and invoke store.getState() in the callback to access the latest state. ' +
+          'See https://redux.js.org/api-reference/store#subscribe(listener) for more details.',
       )
     }
 
@@ -47,7 +47,7 @@ export default function reduxBatch(next) {
       if (isDispatching) {
         throw new Error(
           'You may not unsubscribe from a store listener while the reducer is executing. ' +
-          'See https://redux.js.org/api-reference/store#subscribe(listener) for more details.',
+            'See https://redux.js.org/api-reference/store#subscribe(listener) for more details.',
         )
       }
 
